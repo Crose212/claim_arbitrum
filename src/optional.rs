c61bb6ac7a5a0377a91974285450156028a918fa
Crose212 <102196172+Crose212@users.noreply.github.com>
2023-03-20 14:05:12 +0000

    Changed to waiting for L1 blocks

--- src/optional.rs
+++ src/optional.rs
@@ -1,12 +1,19 @@
+use std::env;
+
 use web3::types::U64;
 use web3::{Web3,transports::WebSocket};
 
-pub async fn wait_for_block(web3s: Web3<WebSocket>) {
+pub async fn wait_for_block() {
+
+    dotenv::dotenv().ok();
+    let websocket = WebSocket::new(&env::var("ETH_SOCKET").unwrap()).await.unwrap();
+    let web3s = Web3::new(websocket);
+
     let needed_block = U64::from(16890400);
     loop {
         let curr_block = web3s.eth().block_number().await.unwrap();
         if needed_block != curr_block {
-            println!("Claim not started yet");
+            println!("Claim not started yet, current block: {:?}", curr_block);
         } else {
             println!("Claim started");
             break;
